f76f4c7534d8951531613b6da6047c05b46d0187
nicebyte <nicebyte@gpfault.net>
2020-05-02 23:23:00 -0700

    winapi call test

---
+++ qbx.asm
@@ -0,0 +1,18 @@
+format PE64 NX GUI 6.0
+entry start
+
+include 'win64_helpers.inc'
+
+HelloStr db "Hello, World!", 0
+
+section '.code' code readable executable
+        start:
+                call64 [MessageBoxA], 0, HelloStr, HelloStr, 0
+                call64 [ExitProcess], 0
+
+section '.idata' import readable writeable
+        import_directory_table KERNEL32, USER32
+        import_functions KERNEL32, ExitProcess
+        import_functions USER32, MessageBoxA
+
+
